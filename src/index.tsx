53860e78aa54a5a7610e8a44e5498cbb316f4a98
silqwer <silqwer@gmail.com>
2023-04-25 15:48:20 +0900

    update: tailwind.config.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
 import routes from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
