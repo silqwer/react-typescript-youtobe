9a80927da1ee4be009760691501d5a5c63563aac
silqwer <silqwer@gmail.com>
2023-04-25 15:01:35 +0900

    update: search bar styling (#8)

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { BsYoutube, BsSearch } from 'react-icons/bs';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
 const SearchHeader = (): JSX.Element => {
   const [text, setText] = useState('');
   const navigate = useNavigate();
@@ -15,21 +16,22 @@ const SearchHeader = (): JSX.Element => {
   }, [keyword]);
 
   return (
-    <header>
-      <div>
-        <BsYoutube />
-        <h1>Youtube</h1>
-      </div>
-      <form onSubmit={handleSubmit}>
+    <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+      <Link to='/' className='flex items-center'>
+        <BsYoutube className='text-4xl text-brand' />
+        <h1 className='font-bold ml-2 text-3xl'>Youtube</h1>
+      </Link>
+      <form className='w-full flex justify-center' onSubmit={handleSubmit}>
         <input
           type='text'
+          className='w-7/12 p-2 outline-none bg-black text-gray-50'
           placeholder='Search...'
           value={text}
           onChange={(e) => {
             setText(e.target.value);
           }}
         />
-        <button>
+        <button className='bg-zinc-600 px-4'>
           <BsSearch />
         </button>
       </form>
