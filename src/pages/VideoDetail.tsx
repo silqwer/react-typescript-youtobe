7886e7604d862baeb8d07345c481dfaf344037a4
silqwer <silqwer@gmail.com>
2023-05-24 08:33:44 +0900

    Feat: Detail Style (#18)

--- src/pages/VideoDetail.tsx
+++ src/pages/VideoDetail.tsx
@@ -11,16 +11,16 @@ const VideoDetail = (): JSX.Element => {
   const { title, channelId, channelTitle, description } = video.snippet;
 
   return (
-    <section>
-      <article>
+    <section className='flex flex-col lg:flex-row'>
+      <article className='basis-4/6'>
         <iframe id='player' width='100%' height='640' src={`http://www.youtube.com/embed/${videoId}`}></iframe>
         <div>
-          <h2>{title}</h2>
+          <h2 className='text-xl font-bold'>{title}</h2>
           <ChannelInfo id={channelId} name={channelTitle} />
-          <pre>{description}</pre>
+          <pre className='whitespace-pre-wrap'>{description}</pre>
         </div>
       </article>
-      <section>
+      <section className='basis-2/6'>
         <RelatedVideos id={videoId} />
       </section>
     </section>
